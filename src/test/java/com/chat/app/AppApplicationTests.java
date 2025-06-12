619afac72b80ed343aef039ebeff6993c5cefd25
Dev Kumar <devkr074@gmail.com>
2025-06-12 09:38:38 +0530

    Initial Commit

---
+++ src/test/java/com/chat/app/AppApplicationTests.java
@@ -0,0 +1,11 @@
+package com.chat.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppApplicationTests {
+	@Test
+	void contextLoads() {
+	}
+}
